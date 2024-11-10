b898b2e344697fae8839aca306f9d3eac8d1a6d6
Ahmed Ali <ahmedaliaay@gmail.com>
2024-11-10 15:55:43 +0200

    Implement collector for ArkiveComposable

--- processor/src/main/java/com/infinum/arkive/processor/subprocessors/ComposeSubprocessor.kt
+++ processor/src/main/java/com/infinum/arkive/processor/subprocessors/ComposeSubprocessor.kt
@@ -2,13 +2,13 @@ package com.infinum.arkive.processor.subprocessors
 
 import com.google.devtools.ksp.processing.CodeGenerator
 import com.google.devtools.ksp.processing.Resolver
-import com.infinum.arkive.processor.collectors.ComposeCollector
+import com.infinum.arkive.processor.collectors.ArkiveComposableCollector
 import com.infinum.arkive.processor.specs.ComposeSpec
 import com.infinum.arkive.processor.validators.ComposeValidator
 
 class ComposeSubprocessor : Subprocessor {
     override fun process(resolver: Resolver, codeGenerator: CodeGenerator) {
-        val collector = ComposeCollector(resolver)
+        val collector = ArkiveComposableCollector(resolver)
         val validator = ComposeValidator()
 
         with(validator.validate(collector.collect())) {
