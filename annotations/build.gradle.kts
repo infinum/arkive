679729e7a39058892363fbe9261f34ac687d2198
Ahmed Ali <ahmedaliaay@gmail.com>
2024-11-16 16:16:24 +0200

    Rename annotations module

--- annotations/build.gradle.kts
+++ annotations/build.gradle.kts
@@ -24,7 +24,7 @@ extra["mavenPublishProperties"] = mapOf(
     "group" to releaseConfig["group"],
     "version" to releaseConfig["version"],
     // TODO - <YOUR-LIBRARY-ARTIFACTID>
-    "artifactId" to "annotaions",
+    "artifactId" to "annotations",
     "repository" to mapOf(
         "url" to sonatype["url"],
         "username" to sonatype["username"],
