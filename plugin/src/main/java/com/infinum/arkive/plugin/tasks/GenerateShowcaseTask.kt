098226a87953f8e471ac4ae51f5a30a4b349fa14
Ahmed Ali <ahmedaliaay@gmail.com>
2024-12-03 22:22:22 +0200

    User Provider and Property

--- plugin/src/main/java/com/infinum/arkive/plugin/tasks/GenerateShowcaseTask.kt
+++ plugin/src/main/java/com/infinum/arkive/plugin/tasks/GenerateShowcaseTask.kt
@@ -5,7 +5,10 @@ import com.infinum.arkive.plugin.services.KSPMetaDataLoader
 import com.infinum.arkive.plugin.services.SnapshotsGrabberImpl
 import com.infinum.arkive.plugin.tasks.shared.BaseSourceTask
 import com.infinum.arkive.plugin.writers.ShowcaseWriterImpl
+import org.gradle.api.file.Directory
 import org.gradle.api.file.FileTree
+import org.gradle.api.provider.Property
+import org.gradle.api.provider.Provider
 import org.gradle.api.tasks.CacheableTask
 import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.InputFiles
@@ -20,14 +23,17 @@ import java.io.File
 internal open class GenerateShowcaseTask : BaseSourceTask() {
 
     @get:OutputDirectory
-    var outputDirectory: File = project.layout.buildDirectory.get().file(
-        FD_GENERATED,
-    ).asFile
+    val outputDirectory: Provider<Directory>
+        get() = project.layout.buildDirectory.dir(
+            FD_GENERATED,
+        )
 
     // Required to invalidate the task on version updates.
-    @Suppress("unused", "ANNOTATION_TARGETS_NON_EXISTENT_ACCESSOR")
+    @Suppress("unused")
     @get:Input
-    private val pluginVersion = "0.0.1" // TODO automate this
+    val pluginVersion: Property<String>
+        get() = project.objects.property(String::class.java)
+            .convention("0.0.1") // TODO automate this
 
     init {
         dependsOn(RECORDING_TASK)
@@ -41,13 +47,15 @@ internal open class GenerateShowcaseTask : BaseSourceTask() {
         val writer = ShowcaseWriterImpl()
 
         val snapshots =
-            snapshotsGrabber.grabAndMoveSnapshots(outputDirectory.resolve(IMAGES_OUTPUT_PATH))
+            snapshotsGrabber.grabAndMoveSnapshots(
+                outputDirectory.get().dir(IMAGES_OUTPUT_PATH).asFile
+            )
         val metadata = metadataLoader.loadMetaData()
 
         val arkiveShowcase = generator.generateShowcase(snapshots, metadata)
         logger.warn("Showcase generated: $arkiveShowcase")
         writer.write(
-            outputDir = outputDirectory,
+            outputDir = outputDirectory.get().asFile,
             showcase = arkiveShowcase,
         )
 
