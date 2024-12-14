8a64f6ab380a2670e25d34a8f5beb0e56266ed99
Ahmed Ali <ahmedaliaay@gmail.com>
2024-12-14 21:31:03 +0200

    Generate json file based on the varaiants

--- metadata/src/main/java/com/inifnum/arkive/metadata/model/ArkiveShowcase.kt
+++ metadata/src/main/java/com/inifnum/arkive/metadata/model/ArkiveShowcase.kt
@@ -4,9 +4,17 @@ import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
+data class ComponentVariant(
+    @SerialName("category") val category: String,
+    @SerialName("variant") val variant: String,
+    @SerialName("snapshotPath") val snapshotPath: String
+)
+
+@Serializable
 data class ShowcaseItem(
     @SerialName("component") val component: Component,
     @SerialName("snapshotPath") val snapshotPath: String,
+    @SerialName("variants") val variants: List<ComponentVariant>
 )
 
 @Serializable
--- plugin/src/main/java/com/infinum/arkive/plugin/generators/ShowcaseGenerator.kt
+++ plugin/src/main/java/com/infinum/arkive/plugin/generators/ShowcaseGenerator.kt
@@ -1,7 +1,9 @@
 package com.infinum.arkive.plugin.generators
 
+import com.inifnum.arkive.metadata.model.ComponentVariant
 import com.inifnum.arkive.metadata.model.ComponentsMetaData
 import com.inifnum.arkive.metadata.model.ShowcaseItem
+import org.gradle.internal.cc.base.logger
 
 interface ShowcaseGenerator {
     fun generateShowcase(snapshots: List<String>, metadata: ComponentsMetaData): List<ShowcaseItem>
@@ -16,6 +18,7 @@ class ShowcaseGeneratorImpl : ShowcaseGenerator {
             ShowcaseItem(
                 component = component,
                 snapshotPath = snapshots.findSnapshot(component.id),
+                variants = snapshots.findVariants(component.id)
             )
         }
         return items
@@ -26,4 +29,31 @@ class ShowcaseGeneratorImpl : ShowcaseGenerator {
             it.endsWith("$id.png")
         } ?: error("Cant find component with id: $id")
     }
+
+    private fun List<String>.findVariants(id: String): List<ComponentVariant> {
+        return filter {
+            it.contains("${id}_")
+        }.map { snapshot ->
+            val variantBlock =
+                snapshot.substring(snapshot.indexOf(id) + id.length + 1, snapshot.indexOf(".png"))
+                    .split("_")
+
+            val category = variantBlock[0]
+            val variant = variantBlock[1]
+            ComponentVariant(
+                category = category,
+                variant = variant,
+                snapshotPath = snapshot
+            )
+        }
+    }
+}
+
+
+fun main() {
+    val path =
+        "images/com.infinum.arkive_ArkiveSnapshotTestGenerator_testAllComposableFunctions_com_infinum_arkive_sample_composables_previewwideroundedbutton_density_3.0.png"
+
+    val id = "com_infinum_arkive_sample_composables_previewwideroundedbutton"
+    println(path)
 }
