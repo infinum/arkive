051faeac3f45b081f2563dddd55f16cf31bdff90
spasepremcheski <spase0706@gmail.com>
2025-05-05 15:23:57 +0200

    Add viewCollector to Subprocessor

--- processor/src/main/java/com/infinum/arkive/processor/subprocessors/ComposeSubprocessor.kt
+++ processor/src/main/java/com/infinum/arkive/processor/subprocessors/ComposeSubprocessor.kt
@@ -4,6 +4,7 @@ import com.google.devtools.ksp.processing.CodeGenerator
 import com.google.devtools.ksp.processing.KSPLogger
 import com.google.devtools.ksp.processing.Resolver
 import com.infinum.arkive.processor.collectors.ArkiveComposableCollector
+import com.infinum.arkive.processor.collectors.ArkiveViewCollector
 import com.infinum.arkive.processor.collectors.PreviewCollector
 import com.infinum.arkive.processor.models.ArkiveOptions
 import com.infinum.arkive.processor.specs.ComposeMetaDataSpec
@@ -18,11 +19,13 @@ class ComposeSubprocessor : Subprocessor {
         options: ArkiveOptions,
     ) {
         val arkiveComposableCollector = ArkiveComposableCollector(resolver, logger)
+        val arkiveViewCollector = ArkiveViewCollector(resolver, logger)
         val previewCollector = PreviewCollector(resolver, logger)
         val validator = ComposeValidator(logger)
 
         val composeHolders = buildSet {
             addAll(arkiveComposableCollector.collect())
+            addAll(arkiveViewCollector.collect())
             if (options.skipPreviews.not()) {
                 addAll(previewCollector.collect())
             }
